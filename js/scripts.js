96cc479f72cd6040c707fcf8426adb131f6fa2b4
lauren wright <wright.lauren20@gmail.com>
2018-08-21 13:51:50 -0400

    added new pages for old work, seperated originals and fanart in folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 lightbox.option({
     "fitImagesInViewport": true;
  })
+
+// $("#twitter".attr("img/twitter-wrap.png", ".../img/icons/twitter-wrap.png"));
+//$( "#twitter", "src" ).replaceWith( "img/icons/twitter-wrap.png" );
+
+// $(document).ready(function(){
+//     if($('footer') {
+//     $('#twitter').html("<img src='.../img/icons/twitter-wrap.png' alt='twitter'>);
+// }
+// });
+const footer = "footer";
+$("#socialFooter").text(footer);
